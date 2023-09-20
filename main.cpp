de256ac6790fc2d86532c6759ca84274641840f0
akachriss <chrisarobles27@gmail.com>
2023-09-20 11:23:44 -0700

    added namespace std

--- main.cpp
+++ main.cpp
@@ -1,20 +1,23 @@
 #include <iostream>
 
+using namespace std;
+
 const int RGB_HEX_LENGTH = 7;
 
 int main(){
-    std::string input;
+    string input;
 
     do{
-        std::cout << "Enter a color in hex format (#RRGGBB):";
-        std::getline(cin, input);
+        cout << "Enter a color in hex format (#RRGGBB):";
+        getline(cin, input);
 
         if( input.size() != RGB_HEX_LENGTH ){
-            std::cout << "Please enter the color in hexadecimal format, starting with # followed by six hex values\n";
+            cout << "Please enter the color in hexadecimal format, starting with # followed by six hex values\n";
         }
-    }while( input.size() != RGB_HEX_LENGTH );
+    }
+    while( input.size() != RGB_HEX_LENGTH );
 
-    std::cout << "Your hex color is: " << input << std::endl;
+    cout << "Your hex color is: " << input << endl;
 
     return 0;
 }
